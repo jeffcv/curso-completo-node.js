9386a71e310c5437e325418f9aad7751f5821a55
Jefferson Vale <jeff.valle@hotmail.com>
2025-02-15 20:12:13 -0300

    Aplicando os core modules fs, url e html

--- 3_CORE_MODULES/4_http_com_url/index.js
+++ 3_CORE_MODULES/4_http_com_url/index.js
@@ -1,8 +1,9 @@
-const http = require("http");
+const http = require("http")
 
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+
   const urlInfo = require('url').parse(req.url, true)
   const name = urlInfo.query.name
 
@@ -10,15 +11,14 @@ const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/html");
 
   if(!name) {
-    
+    res.end('<h1>Preencha o seu nome:</h1><form method="GET"><input type="text" name="name" /><input type="submit" value="Enviar"></form>')
   } else {
-    
+    res.end(`<h1>Seja bem-vindo ${name}!</h1>`)
   }
-
 
 
 });
 
-server.listen(port, () => {
+server.listen(port, '0.0.0.0', () => {
   console.log(`Servidor rodando na porta: ${port}`);
 });
