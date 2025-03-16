7d377f8fa3a1374fefbf15f8011d5bb283733a3c
jeffcv <jeff.valle@hotmail.com>
2025-03-16 10:48:18 -0300

    Implementando a opção Consultar Saldo

--- 5_ACCOUNTS/index.js
+++ 5_ACCOUNTS/index.js
@@ -31,7 +31,7 @@ function operation(){
     } else if(action === 'Depositar') {
         deposit()
     } else if(action === 'Consultar Saldo') {
-
+        getAccountBalance()
     } else if(action === 'Sacar'){
 
     } else if(action === 'Sair'){
@@ -165,4 +165,30 @@ function getAccount(accountName) {
 
     return JSON.parse(accountJSON)
 
+}
+
+// show account balance
+
+function getAccountBalance() {
+    inquirer.prompt([
+        {name: 'accountName',
+        message: 'Qual o nome da conta?'
+        }
+    ]).then((answer) => {
+
+        const accountName = answer['accountName']
+
+        // verify if account exists
+        if(!checkAccount(accountName)){
+            return getAccountBalance()
+        }
+
+        const accountData = getAccount(accountName)
+
+        console.log(chalk.bgBlue.black(
+            `Olá, o saldo da sua conta é de R$${accountData.balance}`
+        )
+    )
+    operation()
+    }).catch(err => console.log(err))
 }
