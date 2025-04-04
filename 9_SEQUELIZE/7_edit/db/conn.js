9e3d6401038d01a2e877d8d909e6bced572d1603
jeffcv <jeff.valle@hotmail.com>
2025-04-04 18:06:39 -0300

    Implementando Editar Usuário com Sequelize

---
+++ 9_SEQUELIZE/7_edit/db/conn.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('nodesequelize', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+// try {
+
+//     sequelize.authenticate()
+//     console.log('Conectamos com sucesso com o Sequelizzzze!')
+
+// } catch (err) {
+//     console.log('Não foi possível conectar!', err)
+// }
+
+module.exports = sequelize
